e75ef1c6241e95603e386253feb8991eb00ca702
saksham vatsa <sakshamvatsa175@gmail.com>
2023-01-18 20:39:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let profilemenu = document.getElementById("profilemenu");
+function toggleMenu(){
+    profilemenu.classList.toggle("open-menu")
+}
+
+    let sideActivity = document.getElementById("sidebarActivity");
+    let moreLink = document.getElementById("showMoreLink");
+
+
+    function toggleActivity(){
+        sideActivity.classList.toggle("open-activity")
+
+        if(sideActivity.classList.contains("open-activity")){
+            
+            moreLink.innerHTML="Show less <b>-<b>";
+        }
+        else{
+            moreLink.innerHTML="Show more  <b>+<b>";
+        }
+    }
